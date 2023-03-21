60d6bae9ed74c975645526c865d830e5697a1f40
tottenham-hyun <hwk3988@gmail.com>
2023-03-22 00:36:35 +0900

    로그인 실패

--- static/js/main.js
+++ static/js/main.js
@@ -1,23 +1,24 @@
-// 검색기능
-$(".input_style").change(async () => {
-  console.log("이벤트");
-  let room_data = await axios.get("/getData");
-  room_data = room_data.data.data;
-  $(".datalist").empty();
-  console.log("방비우기");
-  let new_room_data = [];
+if (sessionStorage.getItem())
+  // 검색기능
+  $(".input_style").change(async () => {
+    console.log("이벤트");
+    let room_data = await axios.get("/getData");
+    room_data = room_data.data.data;
+    $(".datalist").empty();
+    console.log("방비우기");
+    let new_room_data = [];
 
-  for (let i = 0; i < room_data.length; i++) {
-    console.log(room_data[i].title.includes($(".input_style").val()));
-    if (room_data[i].title.includes($(".input_style").val())) {
-      // console.log(room_data[i], input.value);
-      // console.log(new_room_data);
-      new_room_data.push(room_data[i]);
+    for (let i = 0; i < room_data.length; i++) {
+      console.log(room_data[i].title.includes($(".input_style").val()));
+      if (room_data[i].title.includes($(".input_style").val())) {
+        // console.log(room_data[i], input.value);
+        // console.log(new_room_data);
+        new_room_data.push(room_data[i]);
+      }
     }
-  }
-  for (let i = 0; i < new_room_data.length; i++) {
-    console.log("new_room");
-    $(".datalist").append(`
+    for (let i = 0; i < new_room_data.length; i++) {
+      console.log("new_room");
+      $(".datalist").append(`
   <li>
     <div class="list_box">
       <div class="img_box">
@@ -33,13 +34,13 @@ $(".input_style").change(async () => {
     </div>
   </li>
 `);
-  }
+    }
 
-  if ($(".input_style").val() == "") {
-    axios.get("/main", (res) => {
-      console.log(res);
-    });
-  }
-});
+    if ($(".input_style").val() == "") {
+      axios.get("/main", (res) => {
+        console.log(res);
+      });
+    }
+  });
 
 // 달력
