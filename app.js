9ea083cd5a90208d6aa4a28791a7a59e12522289
tottenham-hyun <hwk3988@gmail.com>
2023-03-25 00:27:13 +0900

    서버수정

--- app.js
+++ app.js
@@ -12,18 +12,14 @@ const mongodb = require("mongodb");
 const mongoose = require("mongoose");
 
 var db;
-MongoClient.connect(
-  process.env.DB_URL,
-  { useUnifiedTopology: true },
-  (error, client) => {
-    if (error) return console.log("error");
-    db = client.db("CODINGON");
-    // listen
-    http.listen(process.env.PORT, () => {
-      console.log("listen");
-    });
-  }
-);
+MongoClient.connect(process.env.DB_URL, { useUnifiedTopology: true }, (error, client) => {
+  if (error) return console.log("error");
+  db = client.db("CODINGON");
+  // listen
+  http.listen(process.env.PORT, () => {
+    console.log("listen");
+  });
+});
 
 //passport
 const passport = require("passport");
@@ -32,9 +28,7 @@ const session = require("express-session");
 const { ReplSet } = require("mongodb/lib/core");
 const { currentLogger } = require("mongodb/lib/core/connection/logger");
 
-app.use(
-  session({ secret: "비밀코드", resave: true, saveUninitialized: false })
-);
+app.use(session({ secret: "비밀코드", resave: true, saveUninitialized: false }));
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -65,9 +59,7 @@ app.get("/logout", (req, res, next) => {
       return next(err);
     } else {
       console.log("로그아웃됨");
-      res.send(
-        "<script>location.href='/main'; alert('로그아웃 되었습니다!');</script>"
-      );
+      res.send("<script>location.href='/main'; alert('로그아웃 되었습니다!');</script>");
     }
   });
 });
@@ -167,7 +159,7 @@ app.get("/signup", (req, res) => {
 });
 
 // 방만들기
-app.get("/makeRoom", (req, res) => {
+app.get("/makeRoom", logined, (req, res) => {
   res.render("makeRoom");
 });
 
@@ -192,9 +184,7 @@ app.post("/signup", async (req, result) => {
       result.redirect("/login");
     } else {
       console.log("중복자 발견");
-      result.send(
-        "<script>location.href='/signup'; alert('ID가 중복되었어요!');</script>"
-      );
+      result.send("<script>location.href='/signup'; alert('ID가 중복되었어요!');</script>");
     }
   });
 });
@@ -236,7 +226,7 @@ app.post("/makeRoom", logined, (req, res) => {
 });
 
 // 채팅
-app.get("/chatRoom", (req, res) => {
+app.get("/chatRoom", logined, (req, res) => {
   res.render("chatRoom");
 });
 
